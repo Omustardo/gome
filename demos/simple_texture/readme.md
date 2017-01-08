62ffd0bf87938aa7a68c45206f2f415d3ffe65c1
Omar <omustardo@gmail.com>
2017-01-08 03:02:43 -0800

    Fix rectangle texture coordinates and add demo
    The demo displays a texture on a square

---
+++ demos/simple_texture/readme.md
@@ -0,0 +1,2 @@
+The most basic graphical demo using a single rectangle and texture.
+If running this doesn't work, then something serious is wrong.
--- model/mesh/rect.go
+++ model/mesh/rect.go
@@ -25,28 +25,18 @@ func initializeRect() Mesh {
 	// Store basic rectangle vertices in a buffer.
 	lower, upper := float32(-0.5), float32(0.5)
 	vertices := bytecoder.Float32(binary.LittleEndian,
-		lower, upper, 0,
 		lower, lower, 0,
 		upper, lower, 0,
 		upper, upper, 0,
+		lower, upper, 0,
 	)
 	vertexVBO := gl.CreateBuffer()
-	gl.BindBuffer(gl.ARRAY_BUFFER, vertexVBO)                // Bind the target buffer so we can store values in it. https://www.opengl.org/sdk/docs/man4/html/glBindBuffer.xhtml
-	gl.BufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW) // store values in buffer
+	gl.BindBuffer(gl.ARRAY_BUFFER, vertexVBO)
+	gl.BufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
 	indexBuffer := gl.CreateBuffer()
-
 	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
-	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, bytecoder.Uint32(binary.LittleEndian, 0, 1, 2, 0, 2, 3), gl.STATIC_DRAW)
-
-	textureCoordBuffer := gl.CreateBuffer()
-	gl.BindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer)
-	textureCoordinates := bytecoder.Float32(binary.LittleEndian,
-		0.0, 1.0,
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-	)
-	gl.BufferData(gl.ARRAY_BUFFER, textureCoordinates, gl.STATIC_DRAW)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, bytecoder.Uint16(binary.LittleEndian, 0, 1, 2, 0, 2, 3), gl.STATIC_DRAW)
 
 	// Normals for a 2D object extend perpendicular to the plane it lives on.
 	normals := bytecoder.Float32(binary.LittleEndian,
@@ -58,6 +48,16 @@ func initializeRect() Mesh {
 	normalVBO := gl.CreateBuffer()
 	gl.BindBuffer(gl.ARRAY_BUFFER, normalVBO)
 	gl.BufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW)
+
+	textureCoordBuffer := gl.CreateBuffer()
+	gl.BindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer)
+	textureCoordinates := bytecoder.Float32(binary.LittleEndian,
+		0.0, 0.0,
+		1.0, 0.0,
+		1.0, 1.0,
+		0.0, 1.0,
+	)
+	gl.BufferData(gl.ARRAY_BUFFER, textureCoordinates, gl.STATIC_DRAW)
 
 	return NewMesh(vertexVBO, indexBuffer, normalVBO, gl.TRIANGLES, 6, nil, gl.Texture{}, textureCoordBuffer)
 }
