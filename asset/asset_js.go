6891e94993a54d20f43c5ab6136469c7b3875995
Omar <omustardo@gmail.com>
2017-01-12 21:12:54 -0800

    Catch http GET error in response

--- asset/asset_js.go
+++ asset/asset_js.go
@@ -3,6 +3,7 @@
 package asset
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -14,10 +15,14 @@ func loadFile(path string) ([]byte, error) {
 // httpGet fetches the contents at the given url. This is used as a workaround for loading local assets while on the web.
 // TODO: This prevents `gopherjs build` and then running locally because you can't use http.GET with local files. It has to be with http or https targets.
 // I don't think there's a good way around this. Just don't use `gopherjs build`. Instead use `gopherjs serve` and view the served version.
+// Consider instead returning an io.ReadCloser so large responses are better dealt with.
 func httpGet(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("non-200 status: %s", resp.Status)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
