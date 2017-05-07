10503810732eb13b9977215cde491c8e1427aab2
Omar <omustardo@gmail.com>
2017-05-06 17:15:51 -0700

    Let gome.Initialize set logging options

--- demos/animated_hexagons/main.go
+++ demos/animated_hexagons/main.go
@@ -4,9 +4,7 @@ import (
 	"encoding/binary"
 	"flag"
 	"image/color"
-	"log"
 	"math"
-	"os"
 	"time"
 
 	"github.com/go-gl/mathgl/mgl32"
@@ -37,12 +35,6 @@ var (
 	// use relative asset paths. More explanation in omustardo\gome\asset\asset.go
 	baseDir = flag.String("base_dir", `C:\workspace\Go\src\github.com\omustardo\gome\demos\animated_hexagons`, "All file paths should be specified relative to this root.")
 )
-
-func init() {
-	// log print with .go file and line number.
-	log.SetFlags(log.Lshortfile)
-	log.SetOutput(os.Stdout)
-}
 
 func main() {
 	flag.Parse()
